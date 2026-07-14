b1120c4ce7568430abeaa3263b74894cd3c206a3
Surumi00 <surumika44@gmail.com>
2026-07-14 05:57:00 +0530

    add-car feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Homepage from './assets/components/Homepage'
+import AddCar from './assets/components/AddCar'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Homepage/>
+      <AddCar/>
     </>
   )
 }
